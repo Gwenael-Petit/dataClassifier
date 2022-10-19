a0b2adf4955f062e0bb8c575e422a46755a40be1
Sacha Debusschere <sacha.debusschere.etu@univ-lille.fr>
2022-10-19 16:36:16 +0200

    Add of ICategory 2

---
+++ src/main/java/fr/groupeh6/sae/ICategory.java
@@ -0,0 +1,12 @@
+package fr.groupeh6.sae;
+
+/**
+ * Decrit une categorie. Toute categorie peut etre vue comme un Dataset, elle a
+ * un nom et un ensemble de point qui lui appartiennent.
+ *
+ * Cette interface n'ajoute aucune methode a sa super-interface
+ * <code>IDataset</code>, elle sert seulement a marquer un type.
+ */
+public interface ICategory extends IDataset {
+
+}
