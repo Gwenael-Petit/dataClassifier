1e7c9d6e54867dc506325ad90bc5080417753d85
Gwenael Petit <gwenael.petit.etu@univ-lille.fr>
2022-10-19 18:10:30 +0200

    add IrisPoint

--- src/main/java/fr/groupeh6/sae/points/iris/IrisPoint.java
+++ src/main/java/fr/groupeh6/sae/points/iris/IrisPoint.java
@@ -1,9 +1,30 @@
 package fr.groupeh6.sae.points.iris;
 
+
+import com.opencsv.bean.CsvBindByName;
+
 import fr.groupeh6.sae.columns.IColumn;
 import fr.groupeh6.sae.points.IPoint;
 
 public class IrisPoint implements IPoint {
+	
+	@CsvBindByName(column = "sepal.length")
+	private double sepalLength;
+	
+	@CsvBindByName(column = "sepal.width")
+	private double sepalWidth;
+	
+	@CsvBindByName(column = "petal.length")
+	private double petalLength;
+	
+	@CsvBindByName(column = "petal.width")
+	private double petalWidth;
+	
+	@CsvBindByName(column = "variety")
+	private String variety;
+	
+	
+	public IrisPoint() {}
 
 	public Object getValue(IColumn col) {
 		// TODO Auto-generated method stub
