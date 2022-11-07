700293967a8e6560a5b20a644a3ee8e84476d295
Valentin Dassonville <valentin.dassonville2.etu@univ-lille.fr>
2022-11-07 09:46:43 +0100

    Model méthodes

--- src/main/java/fr/groupeh6/sae/Model.java
+++ src/main/java/fr/groupeh6/sae/Model.java
@@ -1,7 +1,37 @@
 package fr.groupeh6.sae;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.groupeh6.sae.columns.Column;
+import fr.groupeh6.sae.dataset.Dataset;
 import fr.groupeh6.sae.utils.Subject;
 
 public class Model extends Subject {
+	
+	private Dataset dataset;
+	private List<Dataset> categories = new ArrayList<Dataset>();
+	private Column xColumn;
+	private Column yColumn;
+	
+	public void loadFromFile(String dataFile) {
+		
+	}
+	
+	public void loadFromString(String data) {
+		
+	}
+	
+	public void addCategory(Dataset category) {
+		
+	}
+	
+	public List<Dataset> allCategories() {
+		return categories;
+	}
+	
+	public int nbColumns() {
+		return 0;
+	}
 
 }
