0b2c18ea119827f72fd9ad220281cf67e2d225ae
Cedric Larsonnier <cedric.larsonnier.etu@univ-lille.fr>
2022-11-10 10:22:29 +0100

    Update Iris Point normalized

--- src/test/java/fr/groupeh6/sae/points/IrisPointTest.java
+++ src/test/java/fr/groupeh6/sae/points/IrisPointTest.java
@@ -1,21 +1,34 @@
 package fr.groupeh6.sae.points;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
-import fr.groupeh6.sae.columns.Column;
 import fr.groupeh6.sae.columns.StringColumn;
 
 class IrisPointTest {
 
 	@Test
 	void Test() {
-		IrisPoint iris = new IrisPoint(5.1,3.5,1.4,.2,"Setosa");
-		StringColumn col1 = new StringColumn("sepal.length");
-		StringColumn col2 = new StringColumn("sepal.width");
-		assertEquals(5.1,iris.getValue(col1));
-		assertEquals(3.5,iris.getValue(col2));
+		IrisPoint setosa = new IrisPoint(5.1,3.5,1.4,.2,"Setosa");
+		IrisPoint virginica  = new IrisPoint(6.3,3.3,6,2.5,"Virginica");
+		IrisPoint versicolor = new IrisPoint(5.7,2.8,4.1,1.3,"Versicolor");
+		StringColumn spL = new StringColumn("sepal.length");
+		StringColumn spW = new StringColumn("sepal.width");
+		StringColumn ptL = new StringColumn("petal.length");
+		StringColumn ptW = new StringColumn("petal.width");
+		StringColumn var = new StringColumn("variety");
+		
+		assertEquals(5.1,setosa.getValue(spL));
+		assertEquals(3.5,setosa.getValue(spW));
+		assertEquals("Virginica",virginica.getValue(var));
+		assertEquals(1.3,versicolor.getValue(ptW));
+		
+		assertEquals(0.2222,setosa.getNormalizedValue(spL));
+		assertEquals(0.541666,virginica.getNormalizedValue(spW));
+		assertEquals(0.5254237288,versicolor.getNormalizedValue(ptL));
+		assertEquals(0.041666,setosa.getNormalizedValue(ptW));
+		
+		
+
 	}
 
 }
