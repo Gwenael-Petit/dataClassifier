7be1cd6eba4895db9b97d4ca55dab1a609b9e893
Cédric Larsonnier <cedric.larsonnier.etu@univ-lille.fr>
2022-11-17 15:55:47 +0100

    add TitanicDataset

---
+++ src/main/java/fr/groupeh6/sae/model/datas/titanic/TitanicDataset.java
@@ -0,0 +1,40 @@
+package fr.groupeh6.sae.model.datas.titanic;
+
+import java.util.List;
+
+import fr.groupeh6.sae.model.Dataset;
+import fr.groupeh6.sae.model.IPoint;
+import fr.groupeh6.sae.model.columns.Column;
+import fr.groupeh6.sae.model.columns.EnumColumn;
+import fr.groupeh6.sae.model.columns.NumberColumn;
+import fr.groupeh6.sae.model.columns.StringColumn;
+
+public class TitanicDataset extends Dataset{
+	
+	private static final String NAME = "titanic";
+	public static final List<Column> COLUMNS = List.of(
+			new StringColumn("PassengerId"),
+			new NumberColumn("Survived"),
+			new NumberColumn("Pclass"),
+			new StringColumn("Name"),
+			new EnumColumn<EnumSex>("Sex", EnumSex.class),
+			new NumberColumn("Age"),
+			new NumberColumn("SibSp"),
+			new NumberColumn("Parch"),
+			new StringColumn("Ticket"),
+			new NumberColumn("Fare"),
+			new StringColumn("Cabin"),
+			new EnumColumn<EnumEmbarked>("Embarked",EnumEmbarked.class)
+			);
+
+	public TitanicDataset() {
+		super(NAME, COLUMNS);
+	}
+
+	@Override
+	public IPoint getType() {
+		// TODO Auto-generated method stub
+		return new TitanicPoint();
+	}
+
+}
