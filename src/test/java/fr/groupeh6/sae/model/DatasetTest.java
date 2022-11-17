9efb18ded37e58a32cc54df87ffc92e565334413
Sacha Debusschere <sacha.debusschere.etu@univ-lille.fr>
2022-11-17 17:29:15 +0100

    DatasetTest update

--- src/test/java/fr/groupeh6/sae/model/DatasetTest.java
+++ src/test/java/fr/groupeh6/sae/model/DatasetTest.java
@@ -7,6 +7,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.Reader;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -18,6 +19,8 @@ import fr.groupeh6.sae.model.datas.iris.IrisPoint;
 class DatasetTest {
 
 	Dataset irisDataset = new IrisDataset();
+	IrisPoint setosa = new IrisPoint(2.1, 4.1, 3.0, 1.1, "Setosa");
+	IrisPoint virginica = new IrisPoint(3.1, 4.1, 2.1, 4.2, "Virgnica");
 	
 	@Test
 	void test_getName() {
@@ -25,19 +28,28 @@ class DatasetTest {
 	}
 	
 	@Test
-	void test_getNbLines_before_adding() {
+	void test_addLines() {
 		assertEquals(0, irisDataset.getNbLines());
+		irisDataset.addLine(setosa);
+		assertEquals(1, irisDataset.getNbLines());
 	}
+	
 	
 	@Test
 	void test_addAllLine() {
-		List<IrisPoint> elements;		
+		List<IPoint> e = new ArrayList<IPoint>();
+		e.add(setosa);
+		e.add(virginica);
+		irisDataset.addAllLine(e);
+		assertEquals(2, irisDataset.getNbLines());
 	}
 	
 	@Test 
 	void test_setClassifier() {
-		irisDataset.setClassifier(new KnnClassifier(3));
-		assertEquals("Knn", irisDataset.classifier);
+		KnnClassifier knn = new KnnClassifier(3);
+		assertEquals(null, irisDataset.classifier);
+		irisDataset.setClassifier(knn);
+		assertEquals(knn, irisDataset.classifier);
 	}
 
 }
