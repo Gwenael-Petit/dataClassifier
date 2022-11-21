267a00bbede50292f1fc0bdfb617e02ebedcd069
Cedric Larsonnier <cedric.larsonnier.etu@univ-lille.fr>
2022-11-21 11:07:54 +0100

    modif PokemonPointTest

--- src/test/java/fr/groupeh6/sae/model/points/PokemonPointTest.java
+++ src/test/java/fr/groupeh6/sae/model/points/PokemonPointTest.java
@@ -14,16 +14,19 @@ import fr.groupeh6.sae.model.datas.pokemon.EnumType;
 import fr.groupeh6.sae.model.datas.pokemon.PokemonPoint;
 
 class PokemonPointTest {
-	
-	PokemonPoint poke1 = new PokemonPoint("Swablu",40,5120,	255.0,60,600000,45,75,50,EnumType.NORMAL,EnumType.FLYING,1.2,false);
-	PokemonPoint poke2 = new PokemonPoint("Azelf",125,20480,3.0,70,1250000,75,70,115,EnumType.PSYCHIC,0.3,true);
-	PokemonPoint poke3 = new PokemonPoint("Gallade",165,5120,45.0,95,1250000,68,115,110,EnumType.PSYCHIC,EnumType.FIGHTING,52.0,false);
-	
+
+	PokemonPoint poke1 = new PokemonPoint("Swablu", 40, 5120, 255.0, 60, 600000, 45, 75, 50, EnumType.NORMAL,
+			EnumType.FLYING, 1.2, false);
+	PokemonPoint poke2 = new PokemonPoint("Azelf", 125, 20480, 3.0, 70, 1250000, 75, 70, 115, EnumType.PSYCHIC, 0.3,
+			true);
+	PokemonPoint poke3 = new PokemonPoint("Gallade", 165, 5120, 45.0, 95, 1250000, 68, 115, 110, EnumType.PSYCHIC,
+			EnumType.FIGHTING, 52.0, false);
+
 	StringColumn name = (StringColumn) Factory.getInstance().getColumn("name");
 	NumberColumn attack = (NumberColumn) Factory.getInstance().getColumn("attack");
 	NumberColumn baseEggSteps = (NumberColumn) Factory.getInstance().getColumn("base_egg_steps");
 	NumberColumn captureRate = (NumberColumn) Factory.getInstance().getColumn("capture_rate");
-	NumberColumn defense= (NumberColumn) Factory.getInstance().getColumn("defense");
+	NumberColumn defense = (NumberColumn) Factory.getInstance().getColumn("defense");
 	NumberColumn expGrowth = (NumberColumn) Factory.getInstance().getColumn("experience_growth");
 	NumberColumn hp = (NumberColumn) Factory.getInstance().getColumn("hp");
 	NumberColumn spAttack = (NumberColumn) Factory.getInstance().getColumn("sp_attack");
@@ -34,35 +37,34 @@ class PokemonPointTest {
 	BooleanColumn lengendary = (BooleanColumn) Factory.getInstance().getColumn("is_legendary");
 
 	
-
-
-	@Override
-	public String toString() {
-		return "PokemonPointTest [poke1=" + poke1 + ", poke2=" + poke2 + ", poke3=" + poke3 + ", name=" + name
-				+ ", attack=" + attack + ", baseEggSteps=" + baseEggSteps + ", captureRate=" + captureRate
-				+ ", defense=" + defense + ", expGrowth=" + expGrowth + ", hp=" + hp + ", spAttack=" + spAttack
-				+ ", spDefense=" + spDefense + ", type1=" + type1 + ", type2=" + type2 + ", speed=" + speed
-				+ ", lengendary=" + lengendary + "]";
-	}
+	
 	
 	@Test
-	void test_getValue() {
-		assertEquals("Swablu",poke1.getValue(name));
-		assertEquals(125.0,poke2.getValue(attack));
-		assertEquals(5120.0,poke3.getValue(baseEggSteps));
-		assertEquals(255.0,poke1.getValue(captureRate));
-		assertEquals(70.0,poke2.getValue(defense));
-		assertEquals(1250000.0,poke3.getValue(expGrowth));
-		assertEquals(45.0,poke1.getValue(hp));
-		assertEquals(70.0,poke2.getValue(spAttack));
-		assertEquals(110.0,poke3.getValue(spDefense));
-		assertEquals("Swablu",poke1.getValue(name));
-		assertEquals(EnumType.NORMAL,poke1.getValue(type1));
-		assertEquals(null,poke2.getValue(type2));
-		assertEquals(52.0,poke3.getValue(speed));
-		assertEquals(false,poke1.getValue(lengendary));
+	void toStringTest() {
+		assertEquals("Pokemon [name=Swablu, attack=40.0, eggSteps=5120.0, captureRate=255.0, defense=60.0, experience=600000.0, hp=45.0, spAttack=75.0, spDefense=50.0, type1=NORMAL, type2=FLYING, speed=1.2, legendary=false]",poke1.toString());
 	}
-	
-	
+
+	@Test
+	void test_getValue() {
+		assertEquals("Swablu", poke1.getValue(name));
+		assertEquals(125.0, poke2.getValue(attack));
+		assertEquals(5120.0, poke3.getValue(baseEggSteps));
+		assertEquals(255.0, poke1.getValue(captureRate));
+		assertEquals(70.0, poke2.getValue(defense));
+		assertEquals(1250000.0, poke3.getValue(expGrowth));
+		assertEquals(45.0, poke1.getValue(hp));
+		assertEquals(70.0, poke2.getValue(spAttack));
+		assertEquals(110.0, poke3.getValue(spDefense));
+		assertEquals("Swablu", poke1.getValue(name));
+		assertEquals(EnumType.NORMAL, poke1.getValue(type1));
+		assertEquals(null, poke2.getValue(type2));
+		assertEquals(52.0, poke3.getValue(speed));
+		assertEquals(false, poke1.getValue(lengendary));
+	}
+
+	@Test
+	void getNormalizedValueTest() {
+		assertThrows(,poke1.getNormalizedValue(name));
+	}
 
 }
