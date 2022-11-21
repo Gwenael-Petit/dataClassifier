d98a746c24852fe1d2d4e0ec615a171e37216bdb
Sacha Debusschere <sacha.debusschere.etu@univ-lille.fr>
2022-11-21 11:11:59 +0100

    Add NewPointModel & View

---
+++ src/main/java/fr/groupeh6/sae/model/NewPointModel.java
@@ -0,0 +1,15 @@
+package fr.groupeh6.sae.model;
+
+import fr.groupeh6.sae.controllers.MainController;
+import fr.groupeh6.sae.model.utils.Subject;
+import fr.groupeh6.sae.views.NewPointView;
+
+public class NewPointModel extends Subject{
+
+	protected NewPointController npc;
+	
+	public void loadPoint() {
+		MainController.loadPoint();
+	}
+	
+}
---
+++ src/main/java/fr/groupeh6/sae/views/NewPointView.java
@@ -0,0 +1,46 @@
+package fr.groupeh6.sae.views;
+
+import fr.groupeh6.sae.model.NewPointModel;
+import fr.groupeh6.sae.model.utils.Observer;
+import fr.groupeh6.sae.model.utils.Subject;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Window;
+
+public class NewPointView extends AbstractModalView implements Observer{
+
+	protected NewPointModel npm;
+	protected NewPointController npc;
+	
+	
+	public NewPointView(Window owner, NewPointModel npm, NewPointController npc) {
+		super(owner);
+		this.npm = npm;
+		this.npc = npc;
+		npm.attach(this);
+		
+		VBox vBox = new VBox();
+		Label title = new Label("Add a Point");
+		
+		Scene scene = new Scene(vBox, 300, 300);
+		this.setScene(scene);
+		this.setTitle("Add Point");
+		this.show();
+	}
+
+	@Override
+	public void update(Subject subj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Subject subj, Object data) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
