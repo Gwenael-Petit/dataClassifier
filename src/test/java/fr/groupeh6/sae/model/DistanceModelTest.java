51f115d7d9c4d2d969c3145758515d78b9fa9166
Cedric Larsonnier <cedric.larsonnier.etu@univ-lille.fr>
2022-11-30 15:52:42 +0100

    Distance Model test modif

--- src/test/java/fr/groupeh6/sae/model/DistanceModelTest.java
+++ src/test/java/fr/groupeh6/sae/model/DistanceModelTest.java
@@ -7,11 +7,14 @@ import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import fr.groupeh6.sae.model.columns.AbstractColumn;
+import fr.groupeh6.sae.model.columns.NumberColumn;
+
 class DistanceModelTest {
 	MainModel model = new MainModel();
 	protected String sep = System.getProperty("file.separator");
 	protected String path = System.getProperty("user.dir") + sep + "src" + sep + "main" + sep + "resources" + sep + "fr" + sep + "groupeh6" + sep + "sae" + sep + "iris.csv";
-	
+	AbstractColumn abCol = new NumberColumn("test");
 	
 	
 	@BeforeEach
@@ -22,13 +25,45 @@ class DistanceModelTest {
 	@Test
 	void test_getColumns() {
 		DistanceModel distModel = new DistanceModel(model);
-		assertEquals("[sepal.length, sepal.width, petal.length, petal.width, variety]",distModel.getColumns().toString());
+		assertEquals("[Number:sepal.length, Number:sepal.width, Number:petal.length, Number:petal.width, Enum:variety]",distModel.getColumns().toString());
 	}
 	
 	@Test
 	void test_getSelected() {
 		DistanceModel distModel = new DistanceModel(model);
-		assertEquals(null,distModel.getSelected());
+		assertEquals("[Number:sepal.length, Number:sepal.width, Number:petal.length, Number:petal.width]",distModel.getSelected().toString());
 	}
+	
+	@Test
+	void test_is_selected() {
+		DistanceModel distModel = new DistanceModel(model);
+		assertEquals(false,distModel.isSelected(abCol));
+	}
+	
+	@Test
+	void test_get_calcul() {
+		DistanceModel distModel = new DistanceModel(model);
+		assertEquals("Euclidienne",distModel.getCalcul());
+	}
+	
+	@Test
+	void test_set_calcul() {
+		DistanceModel distModel = new DistanceModel(model);
+		assertEquals("Euclidienne",distModel.getCalcul());
+		distModel.setCalcul("Manhattan");
+		assertEquals("Manhattan",distModel.getCalcul());
+	}
+	
+	@Test
+	void test_submit() {
+		DistanceModel distModel = new DistanceModel(model);
+		
+		assertEquals("Euclidienne",model.getDistance().name());
+		distModel.setCalcul("Manhattan");
+		distModel.submit();
+		assertEquals("Manhattan",model.getDistance().name());
+	}
+	
+	
 
 }
