9cec231df95fbc9987975feedbfa4b2d24767c8d
Gwenael Petit <gwenael.petit.etu@univ-lille.fr>
2022-11-30 16:23:57 +0100

    ModelTest 100%

--- src/test/java/fr/groupeh6/sae/model/MainModelTest.java
+++ src/test/java/fr/groupeh6/sae/model/MainModelTest.java
@@ -16,17 +16,12 @@ import fr.groupeh6.sae.model.columns.AbstractColumn;
 import fr.groupeh6.sae.model.columns.NumberColumn;
 import fr.groupeh6.sae.model.columns.StringColumn;
 import fr.groupeh6.sae.model.datas.iris.EnumVariety;
+import fr.groupeh6.sae.model.datas.iris.IrisDataset;
 import fr.groupeh6.sae.model.datas.iris.IrisPoint;
+import fr.groupeh6.sae.model.datas.pokemon.PokemonDataset;
 import fr.groupeh6.sae.model.distance.DistanceEuclidienne;
 
 class MainModelTest {
-	
-	IPoint p1 = new IrisPoint(5.1, 3.5, 1.4, 0.2, EnumVariety.SETOSA);
-	IPoint p2 = new IrisPoint(6.3, 3.3, 6, 2.5, EnumVariety.VIRGINICA);
-	IPoint p3 = new IrisPoint(3.2, 4.2, 4, 1.6, EnumVariety.VIRGINICA);
-	IPoint p4 = new IrisPoint(3.2, 4.2, 4, 1.6, EnumVariety.VIRGINICA);
-	IPoint p5 = new IrisPoint(3.2, 4.2, 4, 1.6, EnumVariety.VIRGINICA);
-	IPoint p6 = new IrisPoint(3.2, 4.2, 4, 1.6, EnumVariety.VIRGINICA);
 	
 	NumberColumn spL = new NumberColumn("sepal.length");
 	NumberColumn spW = new NumberColumn("sepal.width");
@@ -36,22 +31,41 @@ class MainModelTest {
 	DistanceEuclidienne dist = new DistanceEuclidienne(null);
 	
 	protected String sep = System.getProperty("file.separator");
-	protected String path = System.getProperty("user.dir") + sep + "src" + sep + "main" + sep + "resources" + sep + "fr" + sep + "groupeh6" + sep + "sae" + sep + "DataForTest.csv";
+	protected String path = System.getProperty("user.dir") + sep + "src" + sep + "main" + sep + "resources" + sep + "fr" + sep + "groupeh6" + sep + "sae" + sep + "iris.csv";
 	protected File file = new File(path);
 	
 	MainModel model = new MainModel();
 	AbstractColumn classColumn = new StringColumn("variety");
 	Classifier classifier = new KnnClassifier(2, new DistanceEuclidienne(List.of(spL,spW,ptL,ptW)));
+	Classifier classifier2 = new KnnClassifier(1, new DistanceEuclidienne(List.of(spL,spW,ptL,ptW)));
 	IPoint p = new IrisPoint();
 	
 	@Test
 	void test_load_from_file() {
-		
+		try {
+			model.loadFromFile(path, ',', true);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			model.loadFromFile(path, ',', false);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	@Test
 	void test_load_from_String() {
-		
+		try {
+			model.loadFromFile(path, ',', false);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			model.loadFromString("7.1,3.3,6.2,2,\"Virginica\"", ',');
+		} catch (IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 	
 	@Test 
@@ -84,8 +98,9 @@ class MainModelTest {
 		assertTrue(model.categories.get(0).getLines().contains(p));
 		model.setClassifier(classifier);
 		model.setClassClassifier(classColumn);
-		//model.addPoint(p);
-		//assertTrue(model.getCategory("Iris").getLines().contains(p));
+		model.addCategory("SETOSA");
+		model.addPoint(p);
+		assertTrue(model.getCategory("Iris").getLines().contains(p));
 	}
 	
 	@Test
@@ -97,46 +112,90 @@ class MainModelTest {
 		}
 		model.createCategories();
 		assertEquals(2,model.allCategories().size());
+		
+		model.setClassifier(classifier2);
+		model.setClassClassifier(classColumn);
+		model.createCategories();
+		assertEquals(1,model.allCategories().size());
 	}
 	
 	@Test
 	void test_get_category() {
-		
+		assertEquals(null,model.getCategory("Iris"));
+		try {
+			model.loadFromFile(path, ',', false);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
+		model.createCategories();
+		assertEquals(new IrisDataset().getName(),model.getCategory("Iris").getName());
 	}
 	
 	@Test
 	void test_add_category_with_String() {
-		
+		try {
+			model.loadFromFile(path, ',', false);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
+		assertEquals(new IrisDataset().getName(),model.getCategory("Iris").getName());
+		model.addCategory("Pokemon");
+		assertEquals(new PokemonDataset().getName(),model.getCategory("Pokemon").getName());
 	}
 	
 	@Test
 	void test_add_category_with_Dataset() {
-		
+		assertEquals(null,model.getCategory("Iris"));
+		model.addCategory(new IrisDataset());
+		assertEquals(new IrisDataset().getName(),model.getCategory("Iris").getName());
 	}
 	
-	@Test
-	void test_reset_categories() {
-		
-	}
 
 	@Test
 	void test_all_categories() {
-		
+		try {
+			model.loadFromFile(path, ',', false);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
+		assertEquals(1, model.allCategories().size());
 	}
 	
 	@Test
 	void test_robustesse() {
-		
+		try {
+			model.loadFromFile(path, ',', false);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
+		model.setClassifier(classifier2);
+		model.setClassClassifier(classColumn);
+		model.robustesse();
+		assertEquals(0.3,model.getRobustesse(),0.1);
 	}
 	
 	@Test
 	void test_get_robustesse() {
-		
+		assertEquals(0.0,model.getRobustesse());
+		try {
+			model.loadFromFile(path, ',', false);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
+		model.setClassifier(classifier2);
+		model.setClassClassifier(classColumn);
+		model.robustesse();
+		assertEquals(0.3,model.getRobustesse(),0.1);
 	}
 	
 	@Test
 	void test_nbColumns() {
-		
+		try {
+			model.loadFromFile(path, ',', false);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
+		assertEquals(5,model.nbColumns());
 	}
 	
 	@Test
@@ -168,6 +227,17 @@ class MainModelTest {
 	
 	@Test
 	void test_have_train_data_loaded() {
+		assertTrue(model.getTrainDataset() == null);
+		try {
+			model.loadFromFile(path, ',', true);
+		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
+			System.out.println(e.getMessage());
+		}
+		assertFalse(model.getTrainDataset() == null);
+	}
+	
+	@Test
+	void test_get_train_dataset() {
 		assertEquals(null,model.getTrainDataset());
 		try {
 			model.loadFromFile(path, ',', true);
@@ -175,18 +245,7 @@ class MainModelTest {
 			System.out.println(e.getMessage());
 		}
 		model.getTrainDataset();
-		assertEquals(2,model.getTrainDataset().getNbLines());
-	}
-	
-	@Test
-	void test_get_train_dataset() {
-		try {
-			model.loadFromFile(path, ',', true);
-		} catch (NotSameTypeException | IOException | TypeNotRegisteredException e) {
-			System.out.println(e.getMessage());
-		}
-		model.getTrainDataset();
-		assertEquals(2,model.getTrainDataset().getNbLines());
+		assertEquals(150,model.getTrainDataset().getNbLines());
 	}
 	
 	@Test
@@ -201,5 +260,17 @@ class MainModelTest {
 		assertEquals(ptW, model.getyColumn());
 	}
 	
+	@Test
+	void test_set_distance() {
+		assertEquals(null, model.getDistance());
+		model.setDistance(dist);
+		assertEquals(dist, model.getDistance());
+	}
+	
+	@Test
+	void test_get_distance() {
+		model.setDistance(dist);
+		assertEquals(dist, model.getDistance());
+	}
 	
 }
